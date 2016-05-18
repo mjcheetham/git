48d33767d2af881062d6a0a7f0efa8ebf47d668f
Kevin Willford <kewillf@microsoft.com>
2016-05-18 13:40:39 +0000

    gvfs: prevent files to be deleted outside the sparse checkout
    Prevent the sparse checkout to delete files that were marked with
    skip-worktree bit and are not in the sparse-checkout file.
    
    This is because everything with the skip-worktree bit turned on is being
    virtualized and will be removed with the change of HEAD.
    
    There was only one failing test when running with these changes that was
    checking to make sure the worktree narrows on checkout which was
    expected since we would no longer be narrowing the worktree.
    
    Update 2022-04-05: temporarily set 'sparse.expectfilesoutsideofpatterns' in
    test (until we start disabling the "remove present-despite-SKIP_WORKTREE"
    behavior with 'core.virtualfilesystem' in a later commit).
    
    Signed-off-by: Kevin Willford <kewillf@microsoft.com>

--- gvfs.h
+++ gvfs.h
@@ -13,6 +13,7 @@ struct repository;
  */
 #define GVFS_SKIP_SHA_ON_INDEX                      (1 << 0)
 #define GVFS_MISSING_OK                             (1 << 2)
+#define GVFS_NO_DELETE_OUTSIDE_SPARSECHECKOUT       (1 << 3)
 
 int gvfs_config_is_set(struct repository *r, int mask);
 
