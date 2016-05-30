5f312312a4f528dcccdaa020dc305036158d074b
Kevin Willford <kewillf@microsoft.com>
2016-05-30 10:55:53 -0400

    gvfs: optionally skip reachability checks/upload pack during fetch
    While performing a fetch with a virtual file system we know that there
    will be missing objects and we don't want to download them just because
    of the reachability of the commits.  We also don't want to download a
    pack file with commits, trees, and blobs since these will be downloaded
    on demand.
    
    This flag will skip the first connectivity check and by returning zero
    will skip the upload pack. It will also skip the second connectivity
    check but continue to update the branches to the latest commit ids.
    
    Signed-off-by: Kevin Willford <kewillf@microsoft.com>

--- gvfs.h
+++ gvfs.h
@@ -14,6 +14,7 @@ struct repository;
 #define GVFS_SKIP_SHA_ON_INDEX                      (1 << 0)
 #define GVFS_MISSING_OK                             (1 << 2)
 #define GVFS_NO_DELETE_OUTSIDE_SPARSECHECKOUT       (1 << 3)
+#define GVFS_FETCH_SKIP_REACHABILITY_AND_UPLOADPACK (1 << 4)
 
 int gvfs_config_is_set(struct repository *r, int mask);
 
