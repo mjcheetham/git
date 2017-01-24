9c5f7408cf7c132d0316ae653b7477f76dea008f
Kevin Willford <kewillf@microsoft.com>
2017-01-24 17:34:12 +0100

    gvfs: add the core.gvfs config setting
    This does not do anything yet. The next patches will add various values
    for that config setting that correspond to the various features
    offered/required by GVFS.
    
    Signed-off-by: Kevin Willford <kewillf@microsoft.com>
    
    gvfs: refactor loading the core.gvfs config value
    
    This code change makes sure that the config value for core_gvfs
    is always loaded before checking it.
    
    Signed-off-by: Kevin Willford <kewillf@microsoft.com>

--- gvfs.h
+++ gvfs.h
@@ -1,9 +1,13 @@
 #ifndef GVFS_H
 #define GVFS_H
 
+struct repository;
+
 /*
  * This file is for the specific settings and methods
  * used for GVFS functionality
  */
+
+int gvfs_config_is_set(struct repository *r, int mask);
 
 #endif /* GVFS_H */
