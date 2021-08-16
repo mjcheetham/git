b0d5893c2bc4ebefdb09b76a9cb3334fd81d8261
Matthew John Cheetham <mjcheetham@outlook.com>
2021-08-16 11:11:23 +0100

    credential: support extra arbitrary properties
    Alongside the "known" credential properties (protocol, host, path,
    username, password, etc.), add support for arbitrary custom properties
    that can be added by transport helpers, or credential helpers.
    
    This allows for transport-specific information to be passed to external
    credential helpers to aid with fetching or generating new credentials.
    For example, the HTTP transport can include information about response
    headers, the HTTP response code or method.
    
    Conversely, this also permits credential helpers to return extra
    information to be echoed back in subsequent helper calls, or other
    helpers.
    For example, in response to a `get` call a credential helper can include
    some state or context value that Git will then write back in the
    subsequent `store` or `erase` commands.
    
    Any property that is "known" will be parsed in to the existing fields in
    the credential stucture; only unknown fields will fall in to this new
    bucket.
    
    We use a string map to store the key/values and disallow multiple
    properties with the same name (case-insensitive).

--- credential.c
+++ credential.c
@@ -7,6 +7,7 @@
 #include "prompt.h"
 #include "sigchain.h"
 #include "urlmatch.h"
+#include "strmap.h"
 
 void credential_init(struct credential *c)
 {
@@ -22,6 +23,7 @@ void credential_clear(struct credential *c)
 	free(c->username);
 	free(c->password);
 	string_list_clear(&c->helpers, 0);
+	strmap_clear(&c->extra_props, 0);
 
 	credential_init(c);
 }
@@ -37,6 +39,18 @@ int credential_match(const struct credential *want,
 #undef CHECK
 }
 
+void credential_set_prop(struct credential *c, const char *key, const char *val)
+{
+	if (!val) {
+		strmap_remove(&c->extra_props, key, 1);
+		return;
+	}
+
+	void *old_val = (char*)strmap_put(&c->extra_props, key,
+					  (void*)xstrdup(val));
+	if (old_val)
+		free(old_val);
+}
 
 static int credential_from_potentially_partial_url(struct credential *c,
 						   const char *url);
@@ -237,12 +251,13 @@ int credential_read(struct credential *c, FILE *fp)
 			credential_from_url(c, value);
 		} else if (!strcmp(key, "quit")) {
 			c->quit = !!git_config_bool("quit", value);
-		}
-		/*
-		 * Ignore other lines; we don't know what they mean, but
-		 * this future-proofs us when later versions of git do
-		 * learn new lines, and the helpers are updated to match.
-		 */
+		} else
+			/*
+			 * Capture other lines; we don't know what they mean.
+			 * We should preserve them and echo them back to the
+			 * helpers.
+			 */
+			credential_set_prop(c, key, xstrdup(value));
 	}
 
 	strbuf_release(&line);
@@ -263,11 +278,17 @@ static void credential_write_item(FILE *fp, const char *key, const char *value,
 
 void credential_write(const struct credential *c, FILE *fp)
 {
+	struct strmap *props = (struct strmap *)&c->extra_props;
+	struct hashmap_iter props_iter;
+	struct strmap_entry *prop = NULL;
+
 	credential_write_item(fp, "protocol", c->protocol, 1);
 	credential_write_item(fp, "host", c->host, 1);
 	credential_write_item(fp, "path", c->path, 0);
 	credential_write_item(fp, "username", c->username, 0);
 	credential_write_item(fp, "password", c->password, 0);
+	strmap_for_each_entry(props, &props_iter, prop)
+		fprintf(fp, "%s=%s\n", prop->key, (char*)prop->value);
 }
 
 static int run_credential_helper(struct credential *c,
