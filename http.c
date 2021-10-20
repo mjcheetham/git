b3bb5d969c8be1f772d77d1cd5e45d2021c50015
Matthew John Cheetham <mjcheetham@outlook.com>
2021-10-20 17:13:57 +0100

    http: support OAuth2 Bearer tokens directly
    Add support for setting the Authorization header to a Bearer token, such
    as is used in OAuth2. libcurl already supports this option with the
    CURLOPT_XOAUTH2_BEARER.
    
    Rather than introduce a new credential field for the bearer token, we
    reuse the `password` field.

--- http.c
+++ http.c
@@ -493,24 +493,29 @@ static void init_curl_http_auth(CURL *result)
 
 	credential_fill(&http_auth);
 
+	if (strcasecmp("bearer", http_auth.credtype))
+		curl_easy_setopt(result, CURLOPT_XOAUTH2_BEARER,
+			http_auth.password);
+	else {
 #if LIBCURL_VERSION_NUM >= 0x071301
-	curl_easy_setopt(result, CURLOPT_USERNAME, http_auth.username);
-	curl_easy_setopt(result, CURLOPT_PASSWORD, http_auth.password);
+		curl_easy_setopt(result, CURLOPT_USERNAME, http_auth.username);
+		curl_easy_setopt(result, CURLOPT_PASSWORD, http_auth.password);
 #else
-	{
-		static struct strbuf up = STRBUF_INIT;
-		/*
-		 * Note that we assume we only ever have a single set of
-		 * credentials in a given program run, so we do not have
-		 * to worry about updating this buffer, only setting its
-		 * initial value.
-		 */
-		if (!up.len)
-			strbuf_addf(&up, "%s:%s",
-				http_auth.username, http_auth.password);
-		curl_easy_setopt(result, CURLOPT_USERPWD, up.buf);
-	}
+		{
+			static struct strbuf up = STRBUF_INIT;
+			/*
+			* Note that we assume we only ever have a single set of
+			* credentials in a given program run, so we do not have
+			* to worry about updating this buffer, only setting its
+			* initial value.
+			*/
+			if (!up.len)
+				strbuf_addf(&up, "%s:%s",
+					http_auth.username, http_auth.password);
+			curl_easy_setopt(result, CURLOPT_USERPWD, up.buf);
+		}
 #endif
+ 	}
 }
 
 /* *var must be free-able */
