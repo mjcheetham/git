fcaeba7062607a1e020ab9a6de5e2a8e67e5fe28
Johannes Schindelin <Johannes.Schindelin@gmx.de>
2022-08-09 12:22:00 +0200

    codeql: run static analysis as part of CI builds
    CodeQL is GitHub's native offering of a static code analyzer, and hence
    integrates with GitHub Actions better than any other static code
    analyzer.
    
    By default, it comes with a large range of "queries" that test for
    common code patterns that should be avoided.
    
    For now, we only target source code written in C, via the `language:
    cpp` directive. Just in case that other languages should be targeted,
    too, this GitHub workflow job is set up as a matrix job to make that
    easier in the future.
    
    For full documentation, see
    https://docs.github.com/en/code-security/code-scanning/introduction-to-code-scanning/about-code-scanning-with-codeql
    
    Co-authored-by: Pierre Tempel <turbo@github.com>
    Co-authored-by: Arthur Baars <aibaars@github.com>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- ci/install-dependencies.sh
+++ ci/install-dependencies.sh
@@ -128,7 +128,7 @@ ClangFormat)
 	sudo apt-get -q update
 	sudo apt-get -q -y install clang-format
 	;;
-StaticAnalysis)
+StaticAnalysis|codeql)
 	sudo apt-get -q update
 	sudo apt-get -q -y install coccinelle libcurl4-openssl-dev libssl-dev \
 		libexpat-dev gettext make
