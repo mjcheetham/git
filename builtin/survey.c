f9bda2b81bdd70184d10716b24b67f1e3565dbd8
Jeff Hostetler <jeffhostetler@github.com>
2024-04-29 12:47:27 -0400

    survey: calculate stats on refs and print results
    Calculate stats on the set of refs. This includes the number of
    branches, plain and annotated tags, remotes, and etc.  Calculate
    the number of packed vs loose refs.  Calculate the size of the
    set of refnames.
    
    Print results on the console in JSON format.
    
    Add Trace2 logging of the results as a data_json event.
    
    Signed-off-by: Jeff Hostetler <jeffhostetler@github.com>

--- builtin/survey.c
+++ builtin/survey.c
@@ -1,9 +1,13 @@
 #include "builtin.h"
 #include "config.h"
+#include "json-writer.h"
+#include "object-store.h"
 #include "parse-options.h"
 #include "progress.h"
 #include "ref-filter.h"
+#include "refs.h"
 #include "strvec.h"
+#include "trace2.h"
 
 static const char * const survey_usage[] = {
 	N_("git survey [<options>])"),
@@ -166,6 +170,40 @@ static void survey_load_config(void)
 	git_config(survey_load_config_cb, NULL);
 }
 
+/*
+ * Stats on the set of refs that we found.
+ */
+struct survey_stats_refs {
+	uint32_t cnt_total;
+	uint32_t cnt_lightweight_tags;
+	uint32_t cnt_annotated_tags;
+	uint32_t cnt_branches;
+	uint32_t cnt_remotes;
+	uint32_t cnt_detached;
+	uint32_t cnt_other;
+	uint32_t cnt_prefetch;
+
+	uint32_t cnt_symref;
+
+	uint32_t cnt_packed;
+	uint32_t cnt_loose;
+
+	/*
+	 * Measure the length of the refnames.  We can look for potential
+	 * platform limits.  The sum may help us estimate the size of a
+	 * haves/wants conversation, since each refname and a SHA must be
+	 * transmitted.
+	 */
+	size_t len_max_refname;
+	size_t len_sum_refnames;
+};
+
+struct survey_stats {
+	struct survey_stats_refs refs;
+};
+
+static struct survey_stats survey_stats = { 0 };
+
 static void do_load_refs(struct ref_array *ref_array)
 {
 	struct ref_filter filter = REF_FILTER_INIT;
@@ -220,6 +258,80 @@ static void do_load_refs(struct ref_array *ref_array)
 }
 
 /*
+ * Calculate stats on the set of refs that we found.
+ */
+static void do_calc_stats_refs(struct ref_array *ref_array)
+{
+	struct survey_stats_refs *prs = &survey_stats.refs;
+	int k;
+
+	for (k = 0; k < ref_array->nr; k++) {
+		struct ref_array_item *p = ref_array->items[k];
+		struct object_id peeled;
+		size_t len;
+
+		prs->cnt_total++;
+
+		/*
+		 * Classify the ref using the `kind` value.  Note that
+		 * p->kind was populated by `ref_kind_from_refname()`
+		 * based strictly on the refname.  This only knows about
+		 * the basic stock categories and returns FILTER_REFS_OTHERS
+		 * for notes, stashes, and any custom namespaces (like
+		 * "refs/pulls/" or "refs/prefetch/").
+		 */
+		switch (p->kind) {
+		case FILTER_REFS_TAGS:
+			if (!peel_iterated_oid(&p->objectname, &peeled))
+				prs->cnt_annotated_tags++;
+			else
+				prs->cnt_lightweight_tags++;
+			break;
+		case FILTER_REFS_BRANCHES:
+			prs->cnt_branches++;
+			break;
+		case FILTER_REFS_REMOTES:
+			prs->cnt_remotes++;
+			break;
+		case FILTER_REFS_OTHERS:
+			if (starts_with(p->refname, "refs/prefetch/"))
+				prs->cnt_prefetch++;
+			else
+				prs->cnt_other++;
+			break;
+		case FILTER_REFS_DETACHED_HEAD:
+			prs->cnt_detached++;
+			break;
+		default:
+			break;
+		}
+
+		/*
+		 * SymRefs are somewhat orthogonal to the above
+		 * classification (e.g. "HEAD" --> detached
+		 * and "refs/remotes/origin/HEAD" --> remote) so
+		 * our totals will already include them.
+		 */
+		if (p->flag & REF_ISSYMREF)
+			prs->cnt_symref++;
+
+		/*
+		 * Where/how is the ref stored in GITDIR.
+		 */
+		if (p->flag & REF_ISPACKED)
+			prs->cnt_packed++;
+		else
+			prs->cnt_loose++;
+
+		len = strlen(p->refname);
+		prs->len_sum_refnames += len;
+
+		if (len > prs->len_max_refname)
+			prs->len_max_refname = len;
+	}
+}
+
+/*
  * The REFS phase:
  *
  * Load the set of requested refs and assess them for scalablity problems.
@@ -237,8 +349,98 @@ static void survey_phase_refs(void)
 {
 	struct ref_array ref_array = { 0 };
 
+	trace2_region_enter("survey", "phase/refs", the_repository);
 	do_load_refs(&ref_array);
+	trace2_region_leave("survey", "phase/refs", the_repository);
+
+	do_calc_stats_refs(&ref_array);
+
 	ref_array_clear(&ref_array);
+}
+
+static void survey_json(struct json_writer *jw, int pretty)
+{
+	struct survey_stats_refs *prs = &survey_stats.refs;
+
+	jw_object_begin(jw, pretty);
+	{
+		jw_object_inline_begin_object(jw, "refs");
+		{
+			jw_object_intmax(jw, "count", prs->cnt_total);
+
+			jw_object_inline_begin_object(jw, "count_by_type");
+			{
+				if (survey_opts.refs.want_branches)
+					jw_object_intmax(jw, "branches", prs->cnt_branches);
+				if (survey_opts.refs.want_tags) {
+					jw_object_intmax(jw, "lightweight_tags", prs->cnt_lightweight_tags);
+					jw_object_intmax(jw, "annotated_tags", prs->cnt_annotated_tags);
+				}
+				if (survey_opts.refs.want_remotes)
+					jw_object_intmax(jw, "remotes", prs->cnt_remotes);
+				if (survey_opts.refs.want_detached)
+					jw_object_intmax(jw, "detached", prs->cnt_detached);
+				if (survey_opts.refs.want_other)
+					jw_object_intmax(jw, "other", prs->cnt_other);
+
+				/*
+				 * Technically, refs/prefetch/ (and any
+				 * other custom namespace) refs are just
+				 * hidden branches, but we don't include
+				 * them in the above basic categories.
+				 */
+				if (survey_opts.refs.want_prefetch)
+					jw_object_intmax(jw, "prefetch", prs->cnt_prefetch);
+
+				/*
+				 * SymRefs are somewhat orthogonal to
+				 * the above classification
+				 * (e.g. "HEAD" --> detached and
+				 * "refs/remotes/origin/HEAD" -->
+				 * remote) so the above classified
+				 * counts will already include them,
+				 * but it is less confusing to display
+				 * them here than to create a whole
+				 * new section.
+				 */
+				if (prs->cnt_symref)
+					jw_object_intmax(jw, "symrefs", prs->cnt_symref);
+			}
+			jw_end(jw);
+
+			jw_object_inline_begin_object(jw, "count_by_storage");
+			{
+				jw_object_intmax(jw, "loose_refs", prs->cnt_loose);
+				jw_object_intmax(jw, "packed_refs", prs->cnt_packed);
+			}
+			jw_end(jw);
+
+			jw_object_inline_begin_object(jw, "refname_length");
+			{
+				jw_object_intmax(jw, "max", prs->len_max_refname);
+				jw_object_intmax(jw, "sum", prs->len_sum_refnames);
+			}
+			jw_end(jw);
+
+			jw_object_inline_begin_array(jw, "requested");
+			{
+				for (k = 0; k < survey_vec_refs_wanted.nr; k++)
+					jw_array_string(jw, survey_vec_refs_wanted.v[k]);
+			}
+			jw_end(jw);
+		}
+		jw_end(jw);
+	}
+	jw_end(jw);
+}
+
+static void survey_print_results(void)
+{
+	struct json_writer jw = JSON_WRITER_INIT;
+
+	survey_json(&jw, 1);
+	printf("%s\n", jw.json.buf);
+	jw_release(&jw);
 }
 
 int cmd_survey(int argc, const char **argv, const char *prefix)
@@ -253,6 +455,16 @@ int cmd_survey(int argc, const char **argv, const char *prefix)
 	fixup_refs_wanted();
 
 	survey_phase_refs();
+
+	if (trace2_is_enabled()) {
+		struct json_writer jw = JSON_WRITER_INIT;
+
+		survey_json(&jw, 0);
+		trace2_data_json("survey", the_repository, "results", &jw);
+		jw_release(&jw);
+	}
+
+	survey_print_results();
 
 	strvec_clear(&survey_vec_refs_wanted);
 
