14a92fa37cd3c181b7e8b94de84867b69e4b6690
Matthew John Cheetham <mjcheetham@outlook.com>
2025-03-06 11:12:35 +0000

    git.c: add VFS enabled cmd blocking
    Add the ability to block built-in commands based on if the `core.gvfs`
    setting has the `GVFS_USE_VIRTUAL_FILESYSTEM` bit set. This allows us
    to selectively block commands that use the GVFS protocol, but don't use
    VFS for Git (for example repos cloned via `scalar clone` against Azure
    DevOps).
    
    Signed-off-by: Matthew John Cheetham <mjcheetham@outlook.com>

--- git.c
+++ git.c
@@ -33,6 +33,7 @@
 #define NO_PARSEOPT		(1<<5) /* parse-options is not used */
 #define DEPRECATED		(1<<6)
 #define BLOCK_ON_GVFS_REPO	(1<<7) /* command not allowed in GVFS repos */
+#define BLOCK_ON_VFS_ENABLED	(1<<8) /* command not allowed when virtual file system is used */
 
 struct cmd_struct {
 	const char *cmd;
@@ -567,6 +568,9 @@ static int run_builtin(struct cmd_struct *p, int argc, const char **argv, struct
 
 	if (!help && p->option & BLOCK_ON_GVFS_REPO && gvfs_config_is_set(repo, GVFS_BLOCK_COMMANDS))
 		die("'git %s' is not supported on a GVFS repo", p->cmd);
+
+	if (!help && p->option & BLOCK_ON_VFS_ENABLED && gvfs_config_is_set(repo, GVFS_USE_VIRTUAL_FILESYSTEM))
+		die("'git %s' is not supported when using the virtual file system", p->cmd);
 
 	if (run_pre_command_hook(the_repository, argv))
 		die("pre-command hook aborted command");
